d9c6e4a978d2aaa450186eceeaafcd382df3a234
Philippe Bruhat (BooK) <book@cpan.org>
2010-03-20 10:16:12 +0100

    WWW::Mailman: add accessors and constructor code

--- lib/WWW/Mailman.pm
+++ lib/WWW/Mailman.pm
@@ -3,7 +3,95 @@ package WWW::Mailman;
 use warnings;
 use strict;
 
+use Carp;
+use URI;
+use WWW::Mechanize;
+use HTTP::Cookies;
+
 our $VERSION = '0.01';
+
+my @attributes = qw(
+    secure server prefix list
+    email password moderator_password admin_password
+    robot cookie_file
+);
+
+#
+# ACCESSORS / MUTATORS
+#
+
+# generic accessors
+for my $attr (@attributes) {
+    no strict 'refs';
+    *{$attr} = sub {
+        my $self = shift;
+        return $self->{$attr} if !@_;
+        return $self->{$attr} = shift;
+    };
+}
+
+# specialized accessors
+sub uri {
+    my ( $self, $uri ) = @_;
+    if ($uri) {
+        $uri = URI->new($uri);
+
+        # @segments = @prefix, 'mailman', $action, $list, @suffix
+        my ( undef, @segments ) = $uri->path_segments;
+        my @prefix;
+        push @prefix, shift @segments
+            while @segments && $segments[0] ne 'mailman';
+        croak "Invalid URL $uri: no 'mailman' segment"
+            if shift @segments ne 'mailman';
+        croak "Invalid URL $uri: no action"
+            if !shift @segments;
+
+        # just keep the bits we need
+        $self->server( $uri->host );
+        $self->secure( $uri->scheme eq 'https' );
+        $self->prefix( join '/', @prefix );
+        $self->list( shift @segments );
+    }
+
+    # create a generic listinfo URL
+    else {
+        $uri = URI->new();
+        $uri->scheme( $self->secure ? 'https' : 'http' );
+        $uri->host( $self->server );
+        $uri->path( join '/', $self->prefix, 'mailman', 'listinfo',
+            $self->list );
+    }
+    return $uri;
+}
+
+#
+# CONSTRUCTOR
+#
+
+sub new {
+    my ( $class, %args ) = @_;
+
+    # create the object
+    my $self = bless {}, $class;
+
+    # get attributes
+    $self->$_( delete $args{$_} )
+        for grep { exists $args{$_} } @attributes, 'uri';
+
+    # bring in the robot
+    my %mech_options = (
+        agent => "WWW::Mailman/$VERSION",
+        stack_depth => 2,    # make it a Bear of Very Little Brain
+    );
+    $mech_options{cookie_jar} = HTTP::Cookies->new(
+        file           => $self->cookie_file,
+        autosave       => 1,
+        ignore_discard => 1, # Promise me you'll never forget me
+    ) if $self->cookie_file;
+    $self->robot( WWW::Mechanize->new(%mech_options) );
+
+    return $self;
+}
 
 1;
 
